c35f08d6f4069ca08dc9708ec54f81363000140e
Gabrielle Miller-Messner <gabriellemiller-messner@Gabrielles-MacBook-Air.local>
2015-04-20 10:55:49 -0700

    another Swift 1.2 fix

--- Dice Incomplete/Dice Incomplete/RollViewController.swift
+++ Dice Incomplete/Dice Incomplete/RollViewController.swift
@@ -23,7 +23,7 @@ class RollViewController: UIViewController {
     @IBAction func rollTheDice() {
         var controller: DiceViewController
         
-        controller = self.storyboard?.instantiateViewControllerWithIdentifier("DiceViewController") as DiceViewController
+        controller = self.storyboard?.instantiateViewControllerWithIdentifier("DiceViewController") as! DiceViewController
         
         controller.firstValue = self.randomDiceValue()
         controller.secondValue = self.randomDiceValue()
